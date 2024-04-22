ae2f99631d1ba094cf0ff38905695639288656e4
iliannamaniatea <ilianna.mani@gmail.com>
2024-04-22 10:40:40 +0300

    initial

---
+++ src/main/java/gr/accenture/demo/Controller.java
@@ -0,0 +1,19 @@
+package gr.accenture.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class Controller {
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return List.of(new Student(1,"Nikos","Papas"),
+                new Student(2,"Maria","Georgiou"));
+    }
+
+}
